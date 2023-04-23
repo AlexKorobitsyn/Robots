96e3cc65f63fea0161de4d871dd8a6c75d6bda98
AlexKorobitsyn <A.E.Korobitsyn@urfu.me>
2023-04-24 00:18:31 +0500

    Alex Korobitsyn new Window and Robot Fix

--- robots/src/gui/MainApplicationFrame.java
+++ robots/src/gui/MainApplicationFrame.java
@@ -1,79 +1,114 @@
 package gui;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
+import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.beans.PropertyVetoException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
-import javax.swing.JDesktopPane;
-import javax.swing.JFrame;
-import javax.swing.JInternalFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.*;
 
 import log.Logger;
 
 /**
  * Что требуется сделать:
- * 1. Метод создания меню перегружен функционалом и трудно читается. 
+ * 1. Метод создания меню перегружен функционалом и трудно читается.
  * Следует разделить его на серию более простых методов (или вообще выделить отдельный класс).
- *
  */
-public class MainApplicationFrame extends JFrame
-{
+public class MainApplicationFrame extends JFrame {
     private final JDesktopPane desktopPane = new JDesktopPane();
-    
+
     public MainApplicationFrame() {
         //Make the big window be indented 50 pixels from each edge
         //of the screen.
-        int inset = 50;        
+        int inset = 50;
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setBounds(inset, inset,
-            screenSize.width  - inset*2,
-            screenSize.height - inset*2);
+                screenSize.width - inset * 2,
+                screenSize.height - inset * 2);
 
         setContentPane(desktopPane);
-        
-        
+        Rectangle logRec = new Rectangle(10, 10, 300, 800);
+        Rectangle gameRec = new Rectangle(0, 0, 400, 400);
+        HashMap<String, Rectangle> mapCheck = new HashMap<>();
+        mapCheck.put("\"Протокол работы\"", logRec);
+        mapCheck.put("\"Игровое поле\"", gameRec);
         LogWindow logWindow = createLogWindow();
+        GameWindow gameWindow = new GameWindow();
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        try {
+            String string = readUsingFiles(System.getenv("USERPROFILE") + "\\setting.txt");
+            String[] arr = string.split("\n");
+            if (arr[0].equals("1")) {
+                for (String strSave : arr) {
+                    String[] temp1 = strSave.split("(?<=[\"\\d]) ");
+                    if (mapCheck.containsKey(temp1[0])) {
+                        Rectangle tempRectangle = mapCheck.get(temp1[0]);
+                        tempRectangle.x = Integer.parseInt(temp1[2].substring(2));
+                        tempRectangle.y = Integer.parseInt(temp1[3].substring(2));
+                        tempRectangle.width = Integer.parseInt(temp1[4].substring(6));
+                        tempRectangle.height = Integer.parseInt(temp1[5].substring(7));
+                    }
+                }
+            }
+        } catch (IOException ignored) {
+        }
+        logWindow.setBounds(logRec);
         addWindow(logWindow);
 
-        GameWindow gameWindow = new GameWindow();
-        gameWindow.setSize(400,  400);
-        addWindow(gameWindow);
 
+        gameWindow.setBounds(gameRec);
+        addWindow(gameWindow);
         setJMenuBar(generateMenuBar());
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        addWindowListener(new WindowAdapter()
+        {
+            @Override
+            public void windowClosing(WindowEvent e)
+            {
+                System.out.println("Closed");
+                try {
+                    exit();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
     }
-    
-    protected LogWindow createLogWindow()
-    {
+
+    protected LogWindow createLogWindow() {
         LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
-        logWindow.setLocation(10,10);
-        logWindow.setSize(300, 800);
         setMinimumSize(logWindow.getSize());
         logWindow.pack();
         Logger.debug("Протокол работает");
         return logWindow;
     }
-    
-    protected void addWindow(JInternalFrame frame)
-    {
+
+    private static String readUsingFiles(String fileName) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(fileName)));
+    }
+
+    protected void addWindow(JInternalFrame frame) {
         desktopPane.add(frame);
         frame.setVisible(true);
     }
-    
+
 //    protected JMenuBar createMenuBar() {
 //        JMenuBar menuBar = new JMenuBar();
-// 
+//
 //        //Set up the lone menu.
 //        JMenu menu = new JMenu("Document");
 //        menu.setMnemonic(KeyEvent.VK_D);
 //        menuBar.add(menu);
-// 
+//
 //        //Set up the first menu item.
 //        JMenuItem menuItem = new JMenuItem("New");
 //        menuItem.setMnemonic(KeyEvent.VK_N);
@@ -82,7 +117,7 @@ public class MainApplicationFrame extends JFrame
 //        menuItem.setActionCommand("new");
 ////        menuItem.addActionListener(this);
 //        menu.add(menuItem);
-// 
+//
 //        //Set up the second menu item.
 //        menuItem = new JMenuItem("Quit");
 //        menuItem.setMnemonic(KeyEvent.VK_Q);
@@ -91,19 +126,21 @@ public class MainApplicationFrame extends JFrame
 //        menuItem.setActionCommand("quit");
 ////        menuItem.addActionListener(this);
 //        menu.add(menuItem);
-// 
+//
 //        return menuBar;
 //    }
-    
-    private JMenuBar generateMenuBar()
-    {
+
+    private JMenuBar generateMenuBar() {
+        Locale current = new Locale("ru", "RU");
+        ResourceBundle myResources =
+                ResourceBundle.getBundle("text", current);
         JMenuBar menuBar = new JMenuBar();
-        
+
         JMenu lookAndFeelMenu = new JMenu("Режим отображения");
         lookAndFeelMenu.setMnemonic(KeyEvent.VK_V);
         lookAndFeelMenu.getAccessibleContext().setAccessibleDescription(
                 "Управление режимом отображения приложения");
-        
+
         {
             JMenuItem systemLookAndFeel = new JMenuItem("Системная схема", KeyEvent.VK_S);
             systemLookAndFeel.addActionListener((event) -> {
@@ -126,7 +163,7 @@ public class MainApplicationFrame extends JFrame
         testMenu.setMnemonic(KeyEvent.VK_T);
         testMenu.getAccessibleContext().setAccessibleDescription(
                 "Тестовые команды");
-        
+
         {
             JMenuItem addLogMessageItem = new JMenuItem("Сообщение в лог", KeyEvent.VK_S);
             addLogMessageItem.addActionListener((event) -> {
@@ -134,22 +171,59 @@ public class MainApplicationFrame extends JFrame
             });
             testMenu.add(addLogMessageItem);
         }
+        {
 
+            JMenuItem standartExit = new JMenuItem("Выход", KeyEvent.VK_T);
+            standartExit.addActionListener((event) -> {
+                try {
+                    exit();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            testMenu.add(standartExit);
+        }
         menuBar.add(lookAndFeelMenu);
         menuBar.add(testMenu);
         return menuBar;
     }
-    
-    private void setLookAndFeel(String className)
-    {
-        try
-        {
+
+    private void exit() throws IOException {
+        Object[] options = {"Да",
+                "Нет"};
+        int s = JOptionPane.showOptionDialog(null, "Вы уверены что хотите выйти?", "Уведомление", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+        if (s == 0) {
+            File file = new File(System.getenv("USERPROFILE") + "\\setting.txt");
+            if (file.createNewFile()) {
+                System.out.println("File is created!");
+            } else {
+                System.out.println("File already exists.");
+            }
+            FileWriter writer = new FileWriter(file);
+            writer.write("1\n");
+            writer.write("Test data:\n");
+            JInternalFrame[] arr = desktopPane.getAllFrames();
+            for (JInternalFrame var : arr) {
+                writer.write("\"" + var.getTitle() + "\" ");
+                if (var.isIcon())
+                    writer.write("\"Не Свёрнуто\" ");
+                else
+                    writer.write("\"Свёрнуто\" ");
+                Rectangle a = var.getBounds();
+                writer.write("x=" + a.x + " y=" + a.y + " width=" + a.width + " ");
+                writer.write("height=" + a.height + "\n");
+            }
+            writer.close();
+            System.exit(0);
+        }
+    }
+
+    private void setLookAndFeel(String className) {
+        try {
             UIManager.setLookAndFeel(className);
             SwingUtilities.updateComponentTreeUI(this);
-        }
-        catch (ClassNotFoundException | InstantiationException
-            | IllegalAccessException | UnsupportedLookAndFeelException e)
-        {
+        } catch (ClassNotFoundException | InstantiationException
+                 | IllegalAccessException | UnsupportedLookAndFeelException e) {
             // just ignore
         }
     }
